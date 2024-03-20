372a678c3951a306e71f102cc8b1f2a51f14cdc2
RonaldKenneth <rronaldkenneth@gmail.com>
2024-03-20 16:24:33 +0530

    S01E01

---
+++ app.js
@@ -0,0 +1,13 @@
+const hd = React.createElement("div",{id:"heading", summa:"hehehe"},
+[
+    "Maria Joseph",
+    React.createElement("div",{},[
+        "Robert Raja",
+        React.createElement("div",{},[
+            "Ronald Kenneth   n`",
+            " Kevin Romario"
+        ])
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(hd);
